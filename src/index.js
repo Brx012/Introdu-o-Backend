261c07ab08e9872563a73c209c3c41b021d7150b
Brx012 <132697704+Brx012@users.noreply.github.com>
2023-08-24 11:19:32 +0000

    Atividade-14

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import roteadorUsuario from "./routes/usuario.js";
+dotenv.config();      // Requisição do pacote do express
+//index.js
+import { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario, updateUsuario } from "./db/index.js";
+
+
+
+const app = express();              // Instancia o Express
+const port = 3000;                  // Define a porta
+app.use(express.json());
+
+app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
+  res.json({
+    nome: "Bernardo Rafael Sales Saraiva",      // Substitua pelo seu nome
+  });
+  console.log("Rota / solicitada");
+});
+
+
+
+app.listen(port, () => {            // Um socket para "escutar" as requisições
+  console.log(`Serviço escutando na porta:  ${port}`);
+});
+//index.js
+app.use(roteadorUsuario);
