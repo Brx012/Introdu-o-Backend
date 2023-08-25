711e7d7255277b30373898af526df92c9c64bf73
Brx012 <132697704+Brx012@users.noreply.github.com>
2023-08-25 17:39:31 +0000

    Atividade 15

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();   // Requisição do pacote do express
 import roteadorUsuario from "./routes/usuario.js";
+import roteadorLogin from "./routes/login.js";
    
 //index.js
 import { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario, updateUsuario } from "./db/index.js";
@@ -11,7 +12,9 @@ import { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario, updateUsua
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
+app.use(roteadorUsuario);
+app.use(roteadorLogin);
 app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
   res.json({
     nome: "Bernardo Rafael Sales Saraiva",      // Substitua pelo seu nome
@@ -24,5 +27,3 @@ app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
 app.listen(port, () => {            // Um socket para "escutar" as requisições
   console.log(`Serviço escutando na porta:  ${port}`);
 });
-//index.js
-app.use(roteadorUsuario);
